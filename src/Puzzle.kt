5584095029756cd32e282f18f165d40ec0a12746
Matthias Holzinger <Matthias.Holzinger@catalysts.cc>
2018-12-02 13:15:20 +0100

    moves parse to argument, adds multiline interface

--- src/Puzzle.kt
+++ src/Puzzle.kt
@@ -1,17 +1,21 @@
-
-abstract class Puzzle<T>(val day: String, val part: String) {
+abstract class Puzzle<T>(val day: String, val part: String, val parse: (String) -> T = { s -> s as T }) {
     abstract fun solve(input: List<T>): T
-    abstract fun parse(value: String): T
 
-    fun solve(input: List<String>): List<String> {
+    open fun solve(input: List<String>): List<String> {
         return listOf(solve(input.map { parse(it) }).toString())
     }
 }
 
-abstract class IntPuzzle(day: String, part: String) : Puzzle<Int>(day, part) {
-    override fun parse(value: String): Int = value.toInt()
+abstract class MultiLinePuzzle<T>(day: String, part: String, parse: (String) -> T) : Puzzle<T>(day, part, parse) {
+    override fun solve(input: List<T>): T {
+        throw Exception("Not applicable method signature")
+    }
+
+    abstract fun solveMultiline(input: List<T>): List<T>
+
+    override fun solve(input: List<String>): List<String> {
+        return solveMultiline(input.map { parse(it) }).map { it.toString() }
+    }
 }
 
-abstract class StringPuzzle(day: String, part: String) : Puzzle<String>(day, part) {
-    override fun parse(value: String): String = value
-}
+abstract class IntPuzzle(day: String, part: String) : Puzzle<Int>(day, part, String::toInt)
